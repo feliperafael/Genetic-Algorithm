74a70124e088e1069fb6db3f5ddc284afdd9ad42
Felipe Rafael de Souza <feliperafael_95@yahoo.com.br>
2017-04-26 14:45:32 -0300

    Minor modifications

--- src/Individual.h
+++ src/Individual.h
@@ -1,11 +1,22 @@
 #ifndef INDIVIDUAL_H
 #define INDIVIDUAL_H
+#include <string>
 
+#include "Individual.h"
+
+using namespace std;
 
 class Individual
 {
     public:
+        double fitness;
+
         Individual();
+
+        virtual string nameIndividual() = 0; // print name of individual
+        virtual Individual* clone() = 0;
+        virtual void print() = 0;
+
         virtual ~Individual();
 
     protected:
--- src/IndividuoBuilder.h
+++ src/IndividuoBuilder.h
@@ -1,12 +1,14 @@
 #ifndef INDIVIDUOBUILDER_H
 #define INDIVIDUOBUILDER_H
-
+#include "Individual.h"
 
 class IndividuoBuilder
 {
     public:
         IndividuoBuilder();
         virtual ~IndividuoBuilder();
+        virtual Individual * generateIndividuo() = 0;
+        virtual string nameIndividuo() = 0;
 
     protected:
 
